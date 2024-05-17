37405c51261ba0eb36e27e98974e8b51421630c5
wangyao <nagatoismdrama@gmail.com>
2024-05-17 20:47:58 +0800

    new method to combine tree

--- bf-fri/src/mmcs/taptree.rs
+++ bf-fri/src/mmcs/taptree.rs
@@ -1,7 +1,8 @@
-use alloc::vec;
+use alloc::{slice, vec};
 use alloc::vec::Vec;
+use itertools::{Chunk, Itertools};
 use core::ops::{Deref, DerefMut};
-use core::usize;
+use core::{mem, usize};
 
 use bf_scripts::{BfField, EvaluationLeaf, PointsLeaf};
 use bitcoin::taproot::LeafVersion::TapScript;
@@ -53,7 +54,7 @@ impl<const NUM_POLY: usize, F: BfField> PolyCommitTree<F, NUM_POLY> {
             F::sub_group(log2_strict_usize(poly_points)),
             PolynomialType::Eva,
         );
-
+        let mut builder = TreeBuilder::<NUM_POLY>::new();
         for i in 0..evas.values.len() {
             let leaf_script = construct_evaluation_leaf_script::<1, F>(
                 i,
@@ -61,10 +62,10 @@ impl<const NUM_POLY: usize, F: BfField> PolyCommitTree<F, NUM_POLY> {
                 vec![evas.values[i].clone()],
             )
             .unwrap();
-            self.tree.add_leaf(leaf_script);
+            
+           
         }
-
-        self.tree.finalize();
+        self.tree= builder.build_tree();
     }
 
     pub fn commit_rev_points(&mut self, evaluations: Vec<F>, width: usize) {
@@ -72,7 +73,7 @@ impl<const NUM_POLY: usize, F: BfField> PolyCommitTree<F, NUM_POLY> {
         let mut subgroup = F::sub_group(log2_strict_usize(poly_points));
         let leaf_indexs: Vec<usize> = (0..poly_points).into_iter().collect();
         reverse_slice_index_bits(&mut subgroup);
-
+        let mut tree_builder = TreeBuilder::<NUM_POLY>::new();
         for i in (0..poly_points).into_iter().step_by(width) {
             let leaf = PointsLeaf::new(
                 leaf_indexs[i],
@@ -82,16 +83,16 @@ impl<const NUM_POLY: usize, F: BfField> PolyCommitTree<F, NUM_POLY> {
                 subgroup[i + 1],
                 evaluations[i + 1],
             );
-            self.add_leaf(&leaf);
+             self.add_leaf(&mut tree_builder, &leaf)
         }
 
-        self.tree.finalize();
+        self.tree= tree_builder.build_tree();
     }
 
-    pub fn add_leaf(&mut self, leaf: &PointsLeaf<F>) {
+    pub fn add_leaf(&mut self, builder:&mut TreeBuilder<NUM_POLY>,leaf: &PointsLeaf<F>) {
         self.points_leafs.push(leaf.clone());
-        self.tree
-            .add_leaf(leaf.recover_points_euqal_to_commited_point());
+        
+        builder.add_leaf(leaf.recover_points_euqal_to_commited_point());
     }
 
     pub fn get_points_leafs(&self) -> &[PointsLeaf<F>] {
@@ -119,95 +120,160 @@ impl<const NUM_POLY: usize, F: BfField> DerefMut for PolyCommitTree<F, NUM_POLY>
 
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
 pub struct BasicTree<const NUM_POLY: usize> {
-    root_node: Option<NodeInfo>,
-    leaf_count: u64,
-    tree_builder: Option<TreeBuilder>,
-    leaf_indexs: Vec<u64>,
+    pub root_node: Option<NodeInfo>,
+    leaf_count: usize,
+    leaf_indices: Vec<usize>,   
+}
+
+pub struct TreeBuilder<const NUM_POLY:usize>{
+    leaf_count: usize,
+    leaf_indices: Vec<usize>,
+    to_add_leaves:Vec<NodeInfo>,
+}
+
+impl <const NUM_POLY: usize> TreeBuilder<NUM_POLY>{
+    pub fn new()->Self{
+        Self{
+            leaf_count: 0,
+            leaf_indices: Vec::new(),
+            to_add_leaves: Vec::new(),
+        }
+    }
+
+    pub fn add_leaf(&mut self, leaf_script: ScriptBuf) {
+       
+        self.leaf_count += 1;
+        let leaf = NodeInfo::new_leaf_with_ver(leaf_script, TapScript);
+        self.leaf_indices.push(self.leaf_count-1);
+        self.to_add_leaves.push(leaf);
+    } 
+
+
+   
+      /*
+        The leaves_indices are postion info of merkle tree leaves in the taptree.
+        When we building the taptree, it is much easier to work with a dict where the index is 
+        the taptree position and the element is the merkle tree postion. 
+        We flip the dict and save it to the leaf_indices.
+    
+     */
+    pub fn build_tree(&mut self)->BasicTree<NUM_POLY>{
+        let mut working_nodes = self.to_add_leaves.clone();
+        let mut t_idx_to_m_idx = self.leaf_indices.clone(); 
+        
+        while working_nodes.len()>1{
+            //the tuple() method in itertool will drop the elements in Iter if the size is not enough to 
+            //generate a tuple, so we have to save the last node if the size of working node is odd.
+            let mut reminder_node :Option<NodeInfo>= None;
+            if working_nodes.len()%2 ==1{
+                reminder_node = working_nodes.pop();
+            }
+
+
+            let mut node_tuples = working_nodes.into_iter().tuples();
+            let mut todo:Vec<NodeInfo> = Vec::new();
+            let mut a_start_idx =0usize; // will be updated after finishing combining two nodes.
+
+            for (a,b) in node_tuples{
+                let a_leaf_size = a.leaf_nodes().len();
+                let a_end_idx = a_start_idx+a_leaf_size;
+                let b_start_idx = a_end_idx +1;
+                let b_leaf_size = b.leaf_nodes().len();
+                let b_end_idx = b_start_idx+b_leaf_size;
+                let (ret_node,left_first) = NodeInfo::combine_with_order(a, b).unwrap();
+                        
+                todo.push(ret_node);
+
+              
+
+                if !left_first{
+                    let mut temp_a_leaf_indices = vec![0usize;a_leaf_size];
+                    temp_a_leaf_indices.as_mut_slice().copy_from_slice(&t_idx_to_m_idx[a_start_idx..a_end_idx]);
+
+                    let mut temp_b_leaf_indices = vec![0usize;b_leaf_size];
+                    temp_b_leaf_indices.as_mut_slice().copy_from_slice(&t_idx_to_m_idx[b_start_idx..b_end_idx]);
+
+                    t_idx_to_m_idx[a_start_idx..b_end_idx].copy_from_slice(&temp_b_leaf_indices.as_slice());
+                    
+                }
+                a_start_idx+=a_leaf_size+b_leaf_size;
+                
+            }
+            working_nodes = todo;
+            todo = Vec::new();
+        }
+    BasicTree{
+        root_node: Some(working_nodes.into_iter().next().unwrap()),
+        leaf_count:self.leaf_count,
+        leaf_indices:reverse_idx_dict(&t_idx_to_m_idx),
+    }
+    }
+
+}
+
+fn reverse_idx_dict(idx_dict:&Vec<usize>)->Vec<usize>{
+    let mut ret_vec = vec![0usize;idx_dict.len()];
+    for (idx,pos) in idx_dict.iter().enumerate(){
+        ret_vec[*pos]=idx;
+    }
+    ret_vec
 }
 
 impl<const NUM_POLY: usize> BasicTree<NUM_POLY> {
     pub fn new(log_poly_points: usize) -> Self {
         Self {
             root_node: None,
-            leaf_count: 0,
-            tree_builder: Some(TreeBuilder::new(log_poly_points)),
-            leaf_indexs: Vec::new(),
+            leaf_count:0,
+            leaf_indices: Vec::new(),
         }
-    }
-
-    pub fn add_leaf(&mut self, leaf_script: ScriptBuf) {
-        self.leaf_indexs.push(self.leaf_count as u64);
-        self.leaf_count += 1;
-        self.mut_tree_builder().add_leaf(leaf_script);
-    }
-
-    pub fn add_leafs(&mut self, leaf_scripts: Vec<ScriptBuf>) {
-        for leaf_script in leaf_scripts {
-            self.mut_tree_builder().add_leaf(leaf_script);
-        }
-    }
-
-    pub fn isfinalize(&self) -> bool {
-        match self.root_node {
-            Some(_) => true,
-            None => false,
-        }
-    }
+    }     
+    
 
     pub fn root(&self) -> &NodeInfo {
-        assert!(self.isfinalize());
         let root = self.root_node.as_ref().unwrap();
         root
     }
-
-    pub fn tree_builder(&self) -> &TreeBuilder {
-        let builder = self.tree_builder.as_ref().unwrap();
-        builder
-    }
-
-    pub fn mut_tree_builder(&mut self) -> &mut TreeBuilder {
-        let builder = self.tree_builder.as_mut().unwrap();
-        builder
-    }
-
     // This function only support combine trees with same depth
-    pub fn combine_tree(&mut self, other: &mut Self) -> Self {
+    pub fn combine_tree(a:Self, b:Self) -> Self {
+        // perserve indices map before combining two trees.
+         let mut a_leaf_indices = a.leaf_indices.clone();
+         let mut b_leaf_indices = b.leaf_indices.clone();
+         
         let (combined_tree, noswap) =
-            combine_two_nodes(self.root().clone(), other.root().clone()).unwrap();
-        let mut leaf_indexs: Vec<u64> = Vec::new();
-        if !noswap {
-            other.leaf_indexs.append(&mut self.leaf_indexs);
-            leaf_indexs = other.leaf_indexs.clone();
-        } else {
-            self.leaf_indexs.append(&mut other.leaf_indexs);
-            leaf_indexs = self.leaf_indexs.clone();
-        }
-        assert_eq!(
-            leaf_indexs.len(),
-            self.leaf_count() as usize + other.leaf_count() as usize
-        );
+            combine_two_nodes(a.root_node.unwrap(),b.root_node.unwrap()).unwrap();
+        
+        
+
+        let mut a_t_idx_to_m_idx = reverse_idx_dict(&a_leaf_indices);
+        let mut b_t_idx_to_m_idx = reverse_idx_dict(&b_leaf_indices);
+
+        let t_idx_to_m_idx = match noswap{
+            true=>{
+                for b_m_idx in b_t_idx_to_m_idx.iter(){
+                    a_t_idx_to_m_idx.push(*b_m_idx);
+                }
+                a_t_idx_to_m_idx
+            }
+            false=>{
+                for a_m_idx in a_t_idx_to_m_idx.iter(){
+                    b_t_idx_to_m_idx.push(*a_m_idx);
+                }
+                b_t_idx_to_m_idx
+            }
+        };
+        
         Self {
             root_node: Some(combined_tree),
-            leaf_count: self.leaf_count() + other.leaf_count(),
-            tree_builder: None,
-            leaf_indexs: leaf_indexs,
+            leaf_count: t_idx_to_m_idx.len(),
+            leaf_indices: reverse_idx_dict(&t_idx_to_m_idx),
         }
     }
 
-    pub fn finalize(&mut self) {
-        let (new_root_node, new_leaf_indexes) =
-            self.tree_builder.as_mut().unwrap().root_and_leaf_order();
-        self.root_node = Some(new_root_node);
-        self.leaf_indexs = new_leaf_indexes;
-        self.tree_builder = None;
-    }
-
-    pub fn leaf_count(&self) -> u64 {
+    pub fn leaf_count(&self) -> usize {
         self.leaf_count
     }
 
     pub fn leaves(&self) -> LeafNodes {
-        assert!(self.isfinalize());
         let nodes = self.root_node.as_ref().unwrap().leaf_nodes();
         nodes
     }
@@ -222,7 +288,7 @@ impl<const NUM_POLY: usize> BasicTree<NUM_POLY> {
     }
 
     fn index_map(&self, index: usize) -> usize {
-        self.leaf_indexs[index] as usize
+        self.leaf_indices[index] as usize
     }
 
     pub fn get_leaf(&self, index: usize) -> Option<&LeafNode> {
@@ -248,8 +314,7 @@ impl<const NUM_POLY: usize> From<NodeInfo> for BasicTree<NUM_POLY> {
         Self {
             root_node: Some(value),
             leaf_count: 0,
-            tree_builder: None,
-            leaf_indexs: Vec::new(),
+            leaf_indices: Vec::new(),
         }
     }
 }
@@ -265,91 +330,6 @@ pub fn verify_inclusion(root: TapNodeHash, leaf: &LeafNode) -> bool {
 }
 
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
-pub struct TreeBuilder {
-    log_leaves: usize,
-    leaves: Vec<NodeInfo>,
-}
-
-impl TreeBuilder {
-    pub fn new(log_n: usize) -> Self {
-        Self {
-            log_leaves: log_n,
-            leaves: Vec::new(),
-        }
-    }
-
-    pub fn log_leaves(&self) -> usize {
-        self.log_leaves
-    }
-
-    pub fn add_leaf(&mut self, leaf_script: ScriptBuf) {
-        self.leaves
-            .push(NodeInfo::new_leaf_with_ver(leaf_script, TapScript));
-    }
-
-    pub fn root(&mut self) -> NodeInfo {
-        assert!(self.leaves.len() as u32 == 2u32.pow(self.log_leaves as u32));
-        for i in 0..self.log_leaves {
-            self.build_layer((self.log_leaves - i) as u32);
-        }
-        assert!(self.leaves.len() == 1);
-        self.leaves[0].clone()
-    }
-
-    fn build_layer(&mut self, depth: u32) {
-        let nodes_len = self.leaves.len();
-        assert!(nodes_len as u32 == 2u32.pow(depth));
-        for i in (0..nodes_len).step_by(2) {
-            self.leaves[i / 2] =
-                NodeInfo::combine(self.leaves[i].clone(), self.leaves[i + 1].clone()).unwrap();
-        }
-        self.leaves.truncate(nodes_len / 2);
-    }
-
-    pub fn root_and_leaf_order(&mut self) -> (NodeInfo, Vec<u64>) {
-        let mut order = Vec::new();
-
-        let nodes_len = self.leaves.len();
-        for i in 0..nodes_len {
-            order.push(i as u64);
-        }
-
-        assert!(self.leaves.len() as u32 == 2u32.pow(self.log_leaves as u32));
-        for i in 0..self.log_leaves {
-            order = self.build_layer_with_order((self.log_leaves - i) as u32, order);
-        }
-        assert!(self.leaves.len() == 1);
-        (self.leaves[0].clone(), order)
-    }
-
-    fn build_layer_with_order(&mut self, depth: u32, mut order: Vec<u64>) -> Vec<u64> {
-        let chunk_size: usize = 1 << (self.log_leaves as u64 - depth as u64);
-        let mut left_first = false;
-        let nodes_len = self.leaves.len();
-        assert!(nodes_len as u32 == 2u32.pow(depth));
-        for i in (0..nodes_len).step_by(2) {
-            (self.leaves[i / 2], left_first) =
-                NodeInfo::combine_with_order(self.leaves[i].clone(), self.leaves[i + 1].clone())
-                    .unwrap();
-            if !left_first {
-                // let mut order_rep = order.clone();
-                let start = i * chunk_size;
-                let mid = (i + 1) * chunk_size;
-                let end = (i + 2) * chunk_size;
-
-                let temp1 = order[start..mid].to_vec();
-                let temp2 = order[mid..end].to_vec();
-
-                order[start..mid].clone_from_slice(&temp2);
-                order[mid..end].clone_from_slice(&temp1);
-            }
-        }
-        self.leaves.truncate(nodes_len / 2);
-        order
-    }
-}
-
-#[derive(PartialEq)]
 enum PolynomialType {
     Eva,
     Coeff,
@@ -416,7 +396,7 @@ mod tests {
     #[test]
     fn test_tree_builder() {
         type F = BabyBear;
-        const DEPTH: usize = 3;
+        
         let mut coeffs1: Vec<F> = Vec::with_capacity(2u32.pow(DEPTH as u32) as usize);
         for i in 0..2u32.pow(DEPTH as u32) {
             coeffs1.push(F::from_canonical_u32(i));
@@ -425,7 +405,7 @@ mod tests {
         let eva_poly1 = poly1.convert_to_evals_at_subgroup();
         let evas1 = eva_poly1.values();
 
-        let mut tb = TreeBuilder::new(DEPTH);
+        let mut tb = TreeBuilder::new();
 
         for i in 0..evas1.len() {
             let leaf_script = construct_evaluation_leaf_script::<1, F>(
